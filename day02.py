e3007839feb905938adad1df4744de97e9fa595f
hhhwlgns <hhhwlgns@gmail.com>
2025-02-11 10:49:48 +0900

    v0.6 sosu

--- day02.py
+++ day02.py
@@ -1,9 +1,10 @@
-# dan = int(input("Input dan : "))
-# for i in range(1, 10):
-#     print(f"{dan} * {i} = {dan*i}")
+n = int(input("Input number : "))
+count = 0
+for i in range(1, n+1):
+    if n % i == 0:
+        count = count + 1
 
-for dan in range(2, 10):
-    for i in range(1, 10):
-        print(f"{dan} * {i} = {dan*i}")
-
-
+if count == 2:
+     print(f"yes")
+else:
+     print(f"no")

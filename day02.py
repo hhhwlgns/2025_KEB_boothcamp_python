1dcb4bfbcff6edc73952a4db56f4bcc156751e16
hhhwlgns <hhhwlgns@gmail.com>
2025-02-11 13:54:39 +0900

    v1.0문자 열과 ì리스í 리스트, 숫자

--- day02.py
+++ day02.py
@@ -1,23 +1,18 @@
-def is_prime(num) -> bool:
-    """
-    소수 여부를 판정해서 소수면 True 아니면 False를 반환
-    :param num: integer number
-    :return: boolean type
-    """
-    if num >= 2:
-        for i in range(2, int(n ** 0.5) + 1):
-            if num % i == 0:
-               return False
-    else:
-        return False
-
-    return True
-
-# main
-help(is_prime)
-n = int(input("Input number : "))
-
-if is_prime(n): #count == 0:
-     print("yes")
-else:
-     print("no")
+univ = "Inha university"
+print(univ)
+print(univ[5])
+# univ[5] = 'U'  # immutable
+# print(univ)
+subjects = ['python', 'c++', 'linux', 'data structure', 'database']
+print(subjects)
+print(subjects[3])
+subjects[3] = 'data structure & algorithm'  # mutable
+print(subjects)
+# print(0.1)
+# print(1e-1)
+# print(0.01)
+# print(1e-2)
+# print(314.1592)
+# print(0.3141592e3)
+# print(21000)
+# print(21_000)

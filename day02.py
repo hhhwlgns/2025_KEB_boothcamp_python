b16388361d665277f9cbfdeba1ba330814a1f405
hhhwlgns <hhhwlgns@gmail.com>
2025-02-11 13:31:27 +0900

    v0.8 sosu + hamsu

--- day02.py
+++ day02.py
@@ -1,13 +1,23 @@
+def is_prime(num) -> bool:
+    """
+    소수 여부를 판정해서 소수면 True 아니면 False를 반환
+    :param num: integer number
+    :return: boolean type
+    """
+    if num >= 2:
+        for i in range(2, int(n ** 0.5) + 1):
+            if num % i == 0:
+               return False
+    else:
+        return False
+
+    return True
+
+# main
+help(is_prime)
 n = int(input("Input number : "))
-is_prime = True
 
-if n >= 2:
-    for i in range(2, n):
-        if n % i == 0:
-            is_prime = False #count = count + 1
-            break
-
-if is_prime: #count == 0:
+if is_prime(n): #count == 0:
      print("yes")
 else:
      print("no")
